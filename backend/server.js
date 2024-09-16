c685f1491e079aeeddf005617c2dc7b9f2f35a1a
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-09-15 21:25:18 -0400

    Update server.js
    Define PORT by using process.env.PORT || 5000
    
    Signed-off-by: AaronGulman <81993286+AaronGulman@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -31,6 +32,6 @@ app.post('/notes', async (req, res) => {
     res.json(newNote);
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 5000');
 });
